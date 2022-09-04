798039107158ecd16c4f3b421710f64ecbcd6246
zhaowcheng <zhaowcheng@163.com>
2022-09-04 16:28:26 +0800

    添加文章

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220903.1552';
+const cacheName = 'chirpy-20220904.1625';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
